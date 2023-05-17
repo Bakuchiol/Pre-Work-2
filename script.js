05b8e0955eb50f335e26e4427f0578c893ead70b
Bakuchiol <117557313+Bakuchiol@users.noreply.github.com>
2023-05-17 17:23:41 +0000

    script if total is NaN

--- script.js
+++ script.js
@@ -7,12 +7,15 @@ function calculateTip() {
         window.alert('Please enter values')
         return;
     }      //stops from continuing if not filled
-
-    //calculate
+    //Calculate
     let total = (amount * services);
     total = total.toFixed(2); // up to 2 decimals
 
     document.getElementById('tipPercent').innerHTML = '$ ' + total; //shows tip amount
+
+    if (isNaN(total)){
+          document.getElementById('tipPercent').innerHTML = 'Please choose tip amount'; //script if no tip chosen
+    }
 
 }
 
