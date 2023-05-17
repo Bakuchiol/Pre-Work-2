1b6e9179264eae9a421e1be3ca52eb75a51068f0
Bakuchiol <117557313+Bakuchiol@users.noreply.github.com>
2023-05-17 17:40:49 +0000

    window alert

--- script.js
+++ script.js
@@ -5,15 +5,15 @@ function calculateTip() {
     // Validate
     if (amount === '' || services == 0) {
         window.alert('Please enter values')
-        return;
-    }      //stops from continuing if not filled
+        return; // window alert
+    }      
     //Calculate
     let total = (amount * services);
     total = total.toFixed(2); // up to 2 decimals
 
     document.getElementById('tipPercent').innerHTML = '$ ' + total; //shows tip amount
 
-    if (isNaN(total)) {
+    if (isNaN(total)){
           document.getElementById('tipPercent').innerHTML = 'Please choose tip amount'; //script if no tip chosen
     }
 
